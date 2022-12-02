620ea3d28a7b8cb6dcc52a8957a0ef7aad0a15f3
eumii <emmanuel.um@gmail.com>
2022-12-02 10:46:53 +0100

    final submission of lab2.8

--- 2.8 lab.sql
+++ 2.8 lab.sql
@@ -96,13 +96,29 @@ JOIN actor as a
 ON a.actor_id = fa1.actor_id
 ORDER BY fa1.film_id ASC;
 
+# other solutions 
+
 SELECT *
 FROM film_actor as fa1
 JOIN film_actor as fa2
 ON (fa1.film_id = fa2.film_id) AND (fa1.actor_id > fa2.actor_id)
 ORDER BY fa1.film_id ASC;
 
-
+# Final solution
+SELECT
+CONCAT(a1.first_name, " ", a1.last_name) AS actor_1,
+CONCAT(a2.first_name, " ", a2.last_name) AS actor_2,
+film.title
+FROM film_actor AS fa1
+JOIN
+    film_actor AS fa2 ON (fa1.film_id = fa2.film_id)
+        AND (fa1.actor_id > fa2.actor_id)
+        JOIN
+    actor a1 ON (fa1.actor_id = a1.actor_id)
+        JOIN
+    actor a2 ON (fa2.actor_id = a2.actor_id)
+    join film on (fa1.film_id = film.film_id)
+ORDER BY fa1.film_id ASC;
 
 
 
